ab953af47ad80f85e71332e6d86ec0486a10e54d
Santiago Corino <santicorino@gmail.com>
2022-10-12 00:45:16 -0300

    Agregado GET a productos/sabores

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const { getAllProducts } = require("../controllers/allProductosController");
+const { getAllSabores } = require("../controllers/allSaboresController");
 
 const router = Router();
 // Rutas:
 
 router.get("/productos", getAllProducts);
+router.get("/productos/sabores", getAllSabores);
 
 module.exports = router;
---
+++ api/src/utils/allSabores.js
@@ -0,0 +1,20 @@
+const Extras = require("../models/Extras");
+
+async function allSabores() {
+    try {
+        const sabores = await Extras.find({type: "sabores"});
+        if (!sabores.length) {
+          throw new Error(
+            "Error al cargar los Productos desde la DB. Verifique posibles problemas en la relacion del Back con la DB"
+          );
+        } else {
+          return sabores;
+        }
+      } catch (error) {
+        console.error(`${error.name} : ${error.message}`);
+      }
+};
+
+module.exports = {
+    allSabores,
+  };

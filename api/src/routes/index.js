d93cc55cb33f2279ce82522ff93d6a098fe46839
Santiago Corino <santicorino@gmail.com>
2022-10-12 01:22:42 -0300

    Agregados GET a TOPPINGS/FRUTAS/SECOS

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const { getAllProducts } = require("../controllers/allProductosController");
 const { getAllSabores } = require("../controllers/allSaboresController");
+const { getAllToppings } = require("../controllers/allToppingsController");
+const { getAllFrutas } = require("../controllers/allFrutasController");
+const { getAllSecos } = require("../controllers/allSecosController");
 
 const router = Router();
 // Rutas:
 
 router.get("/productos", getAllProducts);
 router.get("/productos/sabores", getAllSabores);
+router.get("/productos/toppings", getAllToppings);
+router.get("/productos/toppings/frutas", getAllFrutas);
+router.get("/productos/toppings/secos", getAllSecos);
 
 module.exports = router;

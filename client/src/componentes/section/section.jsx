dbe506125671ecfadff0037981f29562605f3e66
Santiago Corino <santicorino@gmail.com>
2022-10-12 22:44:27 -0300

    Agregados Section

--- client/src/componentes/section/section.jsx
+++ client/src/componentes/section/section.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Section = () => {
-  return (
+  const productos = useSelector((state)=>state.state.productos);
+    return (
     <div>
-      Holis
+      <h3>Título de la sección</h3>
+      <Order />
+      {
+        productos?.map(p=> {
+            <Cards
+            id={p.id}
+            image={p.image}
+            name={p.name}
+            price={p.price}
+            />
+        })
+      }
     </div>
   )
 }

8008bbc2c574877efe0b18ed79e7502dd1b3c92b
Rolando Oxalc <rolando.oxalc@gmail.com>
2022-10-12 22:35:17 -0500

    Rutas front V1

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 //import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
-import { FlavorsList } from "./componentes/Acordeon/FlavorsList";
-import { ToppingsList } from "./componentes/Acordeon/ToppingsList";
-import { CardDetail } from "./page/detail/CardDetail";
+import { Route } from "react-router-dom";
+import { Switch } from "@chakra-ui/react";
 
 import NavBar from "./componentes/navBar/NavBar";
+import { CardDetail } from "./page/detail/CardDetail";
+import Home from "./page/home/Home";
+import Login from "./page/login/Login";
+import Admin from "./page/users/Admin";
+import Consumer from "./page/Consumer/Consumer";
+import NotFound from "./page/NotFound/NotFound";
+
 
 
 function App() {
   return (
     <div>
-      <BrowserRouter>      
-        {/* <Route></Route> */}
-      </BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<CardDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users/client" element={<Consumer />} />
+        <Route path="/users/admin" element={<Admin />} />
+        <Route path="*" element={<NotFound />} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/page/Consumer/Consumer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Consumer = () => {
+    return (
+        <div>Consumer</div>
+    )
+}
+
+export default Consumer

0f27a9423bb443ec4feb3922ea87e4529b4a7b7b
FrancoPelz <pelaezmontt.fran@gmail.com>
2022-10-16 17:42:33 -0300

    css 404

--- client/src/page/NotFound/NotFound.jsx
+++ client/src/page/NotFound/NotFound.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import e from "./NotFound.module.css"
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className={e.container}>
+            <div className={e.divTitulo}>
+                <h3 className={e.titulo}>Página no encontrada</h3>
+            </div>
+            <div className={e.divTexto}>
+                <strong className={e.texto}>La URL que está intentando ingresar no existe</strong>
+            </div>
+            <div className={e.divBtn}>
+                <Button
+                borderRadius={'full'} 
+                colorScheme='pink' variant='solid'>
+                    <a className={e.btn} href='/'>Regresa al Menu Principal</a>
+                </Button>
+                
+            </div>
+        </div>
     )
 }
 

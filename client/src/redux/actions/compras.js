79430ff8ba97e387af74bec04809980e6f822250
NavarroSofiar <navarro.sofiar@gmail.com>
2022-10-21 15:47:04 -0300

    actualizo action compras que lo modifique sin querer :)

--- client/src/redux/actions/compras.js
+++ client/src/redux/actions/compras.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const addCompraDb = (data) => (dispatch) => {
     // console.log(data)
-    axios.post(`${process.env.REACT_APP_SERVER_URL}/addCompras`, data)
-    .then(res => dispatch(addCompra(res.data)) )
-    .catch(error => console.log(error))
+     console.log(data);
+    axios.post('http://localhost:3800/addCompras', data)
+    
 }

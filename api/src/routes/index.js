a5c23677fb9c3f0d9c68f928c6702d0ed7dc64c0
Marcos <marcosyamilsoria@hotmail.com>
2022-10-21 19:10:31 -0300

    correcciones de bugs en el back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -28,13 +28,15 @@ const {
 const { putExtraController } = require("../controllers/putExtraController");
 const { postCompraController } = require("../controllers/PostCompraController");
 const { getAllTiposController } = require("../controllers/allTiposController");
-const {  getRankingUsuariosCont } = require("../controllers/rankingUsuariosCont");
+const {
+  getRankingUsuariosCont,
+} = require("../controllers/rankingUsuariosCont");
 const { getProductoMasVend } = require("../controllers/rankingProductosCont");
+
 const router = Router();
 
 //Seteo rutas Login
 const loginRouter = require("./login/login.router");
-
 
 // Rutas de PRODUCTOS:
 router.get("/productos", getAllProducts);
@@ -46,7 +48,7 @@ router.get("/productos/helados", getAllHelados);
 router.get("/productos/combos", getAllCombos);
 router.get("/productos/:id", getById);
 router.get("/productos/tipos/:type", filterByType);
-router.get("/tipos", getAllTiposController)
+router.get("/tipos", getAllTiposController);
 
 router.post("/createProducto", postProductosController);
 router.post("/createExtra", postExtraController);
@@ -56,35 +58,28 @@ router.put("/actualizarExtra", putExtraController);
 
 //Rutas de COMPRAS:
 router.get("/compras", getAllCompras);
-router.get("/rankingProductos",getProductoMasVend)
+router.get("/rankingProductos", getProductoMasVend);
 
 router.post("/addCompras", postCompraController);
 
 // Rutas del USUARIO:
 router.get("/listaUsuarios", getAllUsuarios);
-router.get("/rankingUsuarios",getRankingUsuariosCont)
+router.get("/rankingUsuarios", getRankingUsuariosCont);
 
-router.post("/usuarios", validateCreate, postUsuariosController)
-router.post("/createProducto", postProductosController);
+router.post("/usuarios", validateCreate, postUsuariosController);
 router.post("/createExtra", postExtraController);
 
 router.put("/actualizarUsuario", putUsuariosController);
 
 //Rutas de Login
-router.use('/login', loginRouter)
-
-
+router.use("/login", loginRouter);
 
 const PaymentController = require("../controllers/PaymentsController");
 const PaymentService = require("../services/PaymensServices");
-
-
 
 const PaymentInstance = new PaymentController(new PaymentService());
 router.post("/payment", function (req, res, next) {
   PaymentInstance.getPaymentLink(req, res);
 });
-
-
 
 module.exports = router;
--- api/src/utils/rankingProductos.js
+++ api/src/utils/rankingProductos.js
@@ -2,63 +2,59 @@ const Compras = require("../models/Compras");
 
 async function rankingMejoresCompras() {
   try {
-    const ProductosMasvend = await Compras.find(null,{productos:1,_id:0});
-    
-    const ArraydeObjProduc = ProductosMasvend.map(p => {
-    return (
-    p.productos )})
+    const ProductosMasvend = await Compras.find(null, { productos: 1, _id: 0 });
 
-      const reducer = (accumulator, curr) => accumulator.concat(curr);
-      const prodCompradosJuntos= ArraydeObjProduc.reduce(reducer); 
+    const ArraydeObjProduc = ProductosMasvend.map((p) => {
+      return p.productos;
+    });
 
-      if (!prodCompradosJuntos.length) {
+    const reducer = (accumulator, curr) => accumulator.concat(curr);
+    const prodCompradosJuntos = ArraydeObjProduc.reduce(reducer);
+
+    if (!prodCompradosJuntos.length) {
       throw new Error(
-        "Error al cargar los Usuarios desde la DB. Verifique posibles problemas en la relacion del Back con la DB"
+        "Error al cargar los Productos más vendidos desde la DB. Verifique posibles problemas en la relacion del Back con la DB"
       );
     } else {
-        key = "name"
+      key = "name";
 
-        let arr2 = [];
-            
-          prodCompradosJuntos.forEach((x)=>{
-               
-            // Checking if there is any object in arr2
-            // which contains the key value
-             if(arr2.some((val)=>{ return val[key] == x[key] })){
-                 
-               // If yes! then increase the occurrence by 1
-               arr2.forEach((k)=>{
-                 if(k[key] === x[key]){ 
-                   k["occurrence"]++
-                 }
-              })
-                 
-             }else{
-               // If not! Then create a new object initialize 
-               // it with the present iteration key's value and 
-               // set the occurrence to 1
-               let a = {}
-               a[key] = x[key]
-               a["occurrence"] = 1
-               arr2.push(a)
-             }
-          })       
-          const orden = arr2.sort(function (a, b){
-            return (b.occurrence - a.occurrence)
-        })
-       
+      let arr2 = [];
 
-          return orden
-        
+      prodCompradosJuntos.forEach((x) => {
+        // Checking if there is any object in arr2
+        // which contains the key value
+        if (
+          arr2.some((val) => {
+            return val[key] == x[key];
+          })
+        ) {
+          // If yes! then increase the occurrence by 1
+          arr2.forEach((k) => {
+            if (k[key] === x[key]) {
+              k["occurrence"]++;
+            }
+          });
+        } else {
+          // If not! Then create a new object initialize
+          // it with the present iteration key's value and
+          // set the occurrence to 1
+          let a = {};
+          a[key] = x[key];
+          a["occurrence"] = 1;
+          arr2.push(a);
+        }
+      });
+      const orden = arr2.sort(function (a, b) {
+        return b.occurrence - a.occurrence;
+      });
+
+      return orden;
     }
   } catch (error) {
     console.error(`${error.name} : ${error.message}`);
   }
 }
 
-
-
-
 module.exports = {
-    rankingMejoresCompras,
+  rankingMejoresCompras,
 };

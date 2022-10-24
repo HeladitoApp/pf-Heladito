05f6e18a09aecaa2ce17e2e9c9af7a3ac480f2b3
Franco Pelaez <107089825+FrancoPelz@users.noreply.github.com>
2022-10-23 21:25:59 -0300

    Update postUsuarios.js

--- api/src/utils/postUsuarios.js
+++ api/src/utils/postUsuarios.js
@@ -1,12 +1,12 @@
 const Usuarios = require("../models/Usuarios");
-const emailer = require("../controllers/nodeMailer/emailer");
 
 async function postUsuarios(req, res) {
 
-  const { name, apodo, picture, connection, email, phone_number, password, activo, 
-    rol, compras, created_at, update_at} = req.body;
-  /* try {
-    const newInfo = new Usuarios({
+
+  /* const { name, apodo, picture, connection, email, phone_number, password, activo,
+    rol, compras, created_at, update_at } = req.body; /
+
+  / const newInfo = new Usuarios({
     name,
     apodo,
     picture,
@@ -19,7 +19,12 @@ async function postUsuarios(req, res) {
     compras,
     created_at,
     update_at,
-  } = req.body; */
+  }) = req.body; /
+
+  const { name, apodo, picture, connection, email, phone_number, password, activo, 
+    rol, compras, created_at, update_at} = req.body;
+
+
   try {
     const newInfo = new Usuarios({
       name,
@@ -30,28 +35,33 @@ async function postUsuarios(req, res) {
       phone_number,
       password,
       activo,
+      rol,
       compras,
+      created_at,
+      update_at,
     });
     if (!name) {
       throw new Error(
         "Error al crear el ususario. Verifique que la informacion ingresada sea a correcta"
       );
     } else {
-      /* newInfo.name = data.name;
+      / newInfo.name = data.name;
       newInfo.password = data.password;
       newInfo.rol = data.rol;
-      newInfo.mail = data.mail;
+      newInfo.email = data.email;
       newInfo.activo = data.activo;
       newInfo.compras =data.compras */
 
       newInfo.save();
-      emailer.sendMailController(newInfo);
+
+      // emailer.sendMailController(newInfo);
       return newInfo;
     }
   } catch (error) {
-    console.error(`${error.name} : ${error.message}`);
+    console.error(${error.name} : ${error.message});
   }
 }
+
 
 module.exports = {
   postUsuarios,

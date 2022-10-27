614e62a6591f74d21f574a7b3fe07b5266ad2d27
Marcos <marcosyamilsoria@hotmail.com>
2022-10-27 10:09:49 -0300

    cositas

--- api/src/controllers/postUsuariosController.js
+++ api/src/controllers/postUsuariosController.js
@@ -1,17 +1,14 @@
-const Usuarios = require("../models/Usuarios");
 const { postUsuarios } = require("../utils/postUsuarios");
 
-
 async function postUsuariosController(req, res) {
-
   try {
     const result = await postUsuarios(req);
 
     result
       ? res.status(200).send(result)
       : res.status(502).send({
-        message: "Error al subir el Usuario a la BD",
-      });
+          message: "Error al subir el Usuario a la BD",
+        });
   } catch (error) {
     console.error({ message: error.message });
   }
--- api/src/models/Usuarios.js
+++ api/src/models/Usuarios.js
@@ -1,25 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const usuariosSchema = new Schema(
-  {
-    name: { type: String },
-    apodo: {type: String},
-    picture:{type:String},
-    connection:{type:String},
-    email: { type: String},
-    phone_number:{type:Number},
-    password: String,
-    activo: {type: Boolean, default:true}, 
-    rol: {type:String},
-    compras: [{type:Schema.Types.ObjectId, ref: 'Compras'}] ,
-    created_at: {type:String},
-    update_at: {type:String}
-  },
- 
-);
+const usuariosSchema = new Schema({
+  name: { type: String },
+  apodo: { type: String },
+  picture: { type: String },
+  connection: { type: String },
+  email: { type: String },
+  phone_number: { type: Number },
+  password: String,
+  activo: { type: Boolean, default: true },
+  rol: { type: String },
+  compras: [{ type: Schema.Types.ObjectId, ref: "Compras" }],
+  created_at: { type: String },
+  update_at: { type: String },
+});
 
 const Usuarios = mongoose.model("Usuarios", usuariosSchema);
-module.exports = Usuarios; 
-
-
+module.exports = Usuarios;

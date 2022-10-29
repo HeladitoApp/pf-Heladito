b28637842f5c9e34caa9a084e274a2b1d8dcb795
NavarroSofiar <navarro.sofiar@gmail.com>
2022-10-29 04:08:06 -0300

    corrijo el bot para que no discrimine y agrego la ubicación en map

--- client/src/componentes/Map/map.jsx
+++ client/src/componentes/Map/map.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import s from "./map.module.css";
-import { Box } from '@chakra-ui/react';
-
-const Marcador = () => <div className={s.marcador}></div>;
+import React , { useState } from 'react'
+import { GoogleMap, LoadScript ,InfoWindow, Marker} from '@react-google-maps/api';
+/* import s from "./map.module.css";
+import { Box } from '@chakra-ui/react'; */
+const markers = [
+  {
+    id: 1,
+    name: "Sucursal HeladitosApp Franco",
+    position: { lat: -34.604527, lng: -58.397300 }
+  },
+  {
+    id: 2,
+    name: "Sucursal HeladitosApp Sofi",
+    position: { lat: -26.829801 , lng: -65.203815 }
+  },
+  {
+    id: 3,
+    name: "Los Angeles, California",
+    position: { lat: 34.052235, lng: -118.243683 }
+  },
+  {
+    id: 4,
+    name: "New York, New York",
+    position: { lat: 40.712776, lng: -74.005974 }
+  }
+];
 
 export default function Mapa() {
+  const [activeMarker, setActiveMarker] = useState(null);
+
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
 
 const containerStyle = {
     width: '250px',
@@ -18,20 +46,36 @@ const containerStyle = {
   };
 
   return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyAdFvEuvfmpHiw9qSye4Cyj6Yedc7zOS84"
+   <LoadScript
+      googleMapsApiKey="AIzaSyBnThDdJuBYKQu3x9N-gjf2LaFJ3GeVhjY"
     >
       <GoogleMap
+      onClick={() => setActiveMarker(null)}
         mapContainerStyle={containerStyle}
         center={center}
         zoom={12}
       >
-        <Marcador
-          lat={-34.60376}
-          lng={-58.38162}
-          text="Heladitos"
-        />
+        {markers.map(({ id, name, position }) => (
+        <Marker
+        
+          key={id}
+          position={position}
+          onClick={() => handleActiveMarker(id)}
+        >
+            {activeMarker === id ? (
+            <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+              <div>{name}</div>
+            </InfoWindow>
+          ) : null}
+        </Marker>
+        ))}
       </GoogleMap>
-    </LoadScript>
+    </LoadScript>  
+  
+ /*      <div className="google-map-code">
+      <iframe src="https://www.google.com/maps/d/embed?mid=1zTnce5J3-2eOEJot3P_BOQp6NSE8t8c&ehbc=2E312F" width="250px" height="250px"></iframe>
+      </div>  */
+  
+
   )
 }

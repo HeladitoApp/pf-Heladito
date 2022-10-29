a4f2d7cd2ca18c552e3e1c859fcd79d6ddbbae83
Rolando Quispe <rolando.oxalc@gmail.com>
2022-10-28 22:53:45 -0500

    Corregido el log out con refresh

---
+++ client/src/componentes/DataTables/BarChart.jsx
@@ -0,0 +1,69 @@
+import { Box, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import Chart from "react-apexcharts";
+import colors from "../../theme/colors";
+
+const BarChart = ({ title, data }) => {
+
+    const dispatch = useDispatch();
+    const types = useSelector((state) => state.state.types)
+
+    const [options, setOptions] = useState({
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 4,
+                horizontal: true,
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        xaxis: {
+            categories: types,
+        },
+        responsive: [
+            {
+                breakpoint: 1000,
+                options: {
+                    plotOptions: {
+                        bar: {
+                            horizontal: false
+                        }
+                    },
+                    legend: {
+                        position: "bottom"
+                    }
+                }
+            }
+        ],
+        colors: [colors.rosado.original]
+
+    })
+
+    const [series, setSeries] = useState([data])
+    useEffect(() => {
+
+    })
+
+    return (
+        <Stack my={3} boxShadow='dark-lg' p={15}>
+            <Text
+                fontSize='xl'
+                as='b'
+                align={'center'}
+            >
+                {title}
+            </Text>
+            <Chart options={options} series={series} type={'bar'} width='150%' align='center'
+                height='100%' />
+        </Stack>
+    )
+}
+
+export default BarChart

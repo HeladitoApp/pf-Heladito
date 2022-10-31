fa59e4d09a378f48d907dd7efb0e4201877154ff
cristian cobo <ccobo1405@gmail.com>
2022-10-31 17:35:31 -0500

    Actulizacion de rutas en el front

--- client/src/redux/actions/addPayment.js
+++ client/src/redux/actions/addPayment.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const addPayment = (data) => (dispatch) => {
     console.log(data);
-    axios.post('http://localhost:3800/payment', data)
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/payment`, data)
     .then(res => window.location.replace(res.data))
     .catch(error => console.log(error))
 }
